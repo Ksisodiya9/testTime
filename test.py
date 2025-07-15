67e8fb8ed4a27f5719b588a16109d1546521c533
Kshitij <ksisodiya9@gmail.com>
2025-07-15 20:25:35 +0530

    test sch

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
 from datetime import datetime
-import time
+# import time
 
 if __name__ == "__main__":
-    
-    MAX_LOOP = 1000
-    INTERVAL = 900 #15 minutes
 
-    i = 0
-    while i<=MAX_LOOP:
-        print(datetime.now())
-        time.sleep(INTERVAL)
-        i+=1
+    print(datetime.now())
